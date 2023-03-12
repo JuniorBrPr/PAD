951a2dd651a22094aec9b840c7116674d8668d1b
Jaydeng <jaydengunhan@gmail.com>
2023-03-12 23:07:06 +0100

    Added logic for calculating score

--- server/routes/activityRoute.js
+++ server/routes/activityRoute.js
@@ -46,8 +46,10 @@ class ActivityRoute {
     #getUserScore() {
         this.#app.get("/activity/:userId/score", async (req, res) => {
             try {
+                let totalScore = 0;
 
                 //Here we are requesting all goals with an end_date, Having an end date means being completed
+
                 const data = await this.#databaseHelper.handleQuery({
                     query: "SELECT ug.userId, a.activity_name, g.difficulty" +
                         "FROM userGoal ug" +
@@ -58,8 +60,34 @@ class ActivityRoute {
                     values: [req.params.userId]
                 });
 
+                for (const item of data) {
+
+                    /**
+                     * Switch statement to add to totalScore that we give back to the user.
+                     * Will be used to calculate future goals
+                     */
+
+                    switch (item.difficulty) {
+                        case 1:
+                            totalScore += this.#activityCodes.VERY_EASY_COEF;
+                            break;
+                        case 2:
+                            totalScore += this.#activityCodes.EASY_COEF;
+                            break;
+                        case 3:
+                            totalScore += this.#activityCodes.NORMAL_COEF;
+                            break;
+                        case 4:
+                            totalScore += this.#activityCodes.HARD_COEF;
+                            break;
+                        case 5:
+                            totalScore += this.#activityCodes.VERY_HARD_COEF;
+                            break;
+                    }
+                }
+
                 //just give all data back as json, could also be empty
-                res.status(this.#errorCodes.HTTP_OK_CODE).json(data);
+                res.status(this.#errorCodes.HTTP_OK_CODE).json(totalScore);
             } catch(e) {
                 res.status(this.#errorCodes.BAD_REQUEST_CODE).json({reason: e})
             }
