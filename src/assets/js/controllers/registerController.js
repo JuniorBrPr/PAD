43b4f5b3457986692ab3b568c4574cadfb0379b2
hanan Ouardi <hanan.ouardi@hva.nl>
2023-03-18 17:53:12 +0100

    Registratie pagina || connectie met backend werkt.

--- src/assets/js/controllers/registerController.js
+++ src/assets/js/controllers/registerController.js
@@ -3,6 +3,8 @@
  */
 
 import {Controller} from "./controller.js";
+import {RegisterRepository} from "../repositories/registerRepository.js";
+import {App} from "../app.js";
 
 
 
@@ -10,15 +12,52 @@ import {Controller} from "./controller.js";
 
 export class RegisterController extends Controller {
     #registerView
+    #registerRepository;
 
     constructor() {
         super();
 
+        this.#registerRepository = new RegisterRepository();
         this.#setupView()
     }
 
     async #setupView() {
         this.#registerView = await super.loadHtmlIntoContent("html_views/register.html")
+
+        this.#registerView.querySelector(".btn").addEventListener("click",
+            (event) => this.#saveRegister(event));
+
+    }
+
+    async #saveRegister(event){
+        event.preventDefault();
+
+        const firstName = this.#registerView.querySelector("#inputFirstName").value;
+        const lastName = this.#registerView.querySelector("#inputLastName").value;
+        const email = this.#registerView.querySelector("#inputEmail").value;
+        const password = this.#registerView.querySelector("#inputPassword").value;
+        const confirmPassword = this.#registerView.querySelector("#inputConfirmPassword").value;
+
+        const errorBox = this.#registerView.querySelector(".error")
+        if(firstName.length === 0 || lastName.length === 0 || email.length === 0 || password.length === 0 || confirmPassword.length === 0){
+            errorBox.innerHTML = "U moet eerst uw gegevens invullen";
+
+            return ;
+        }
+            errorBox.innerHTML = "";
+            console.log(firstName + lastName + email + password + confirmPassword);
+            try {
+                errorBox.innerHTML = "";
+                const data = await this.#registerRepository.createRegister(firstName, lastName, email, password, confirmPassword);
+                console.log(data);
+
+                if(data.id){
+                    App.loadController(App.CONTROLLER_WELCOME);
+                }
+            }catch(e){
+                errorBox.innerHTML = "Er is iets fout gegaan!"
+                console.log(e)
+            }
 
 
     }
