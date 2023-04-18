ef8dae885c51f86d653b2c57dc2b9498ef4c2d09
hanan Ouardi <hanan.ouardi@hva.nl>
2023-04-18 11:59:06 +0200

    .

--- server/routes/weekPlanningRoutes.js
+++ server/routes/weekPlanningRoutes.js
@@ -1,4 +1,21 @@
 /**
+ *
  * @author Hanan Ouardi
  */
 
+class WeekPlanningRoutes {
+    #errorCodes = require("../framework/utils/httpErrorCodes")
+    #databaseHelper = require("../framework/utils/databaseHelper")
+    #app
+
+
+    constructor(app) {
+        this.#app = app;
+
+        this.#dataWeekPlanning()
+    }
+
+    #dataWeekPlanning(){
+
+    }
+}
--- src/assets/js/repositories/weekPlanningRepository.js
+++ src/assets/js/repositories/weekPlanningRepository.js
@@ -1,2 +1,22 @@
+/**
+ * Ontvangt de data van de database om in de planning te zetten
+ *
+ * @author Hanan Ouardi
+ */
+import {NetworkManager} from "../framework/utils/networkManager.js";
+
+export class PlanningRepository {
+    #networkManager;
+    #route;
 
 
+    constructor() {
+         this.#route = "/planning"  /**aanpassen*/
+         this.#networkManager = new NetworkManager();
+    }
+
+    createPlanning() {
+        this.#networkManager.doRequest(this.#route, "GET", {})
+    }
+
+}
