e6ddf210483afa0aaa6f85f434f8d48c7dbeba9a
Joey van der Poel <Joey.van.der.poel@hva.nl>
2023-05-17 10:32:39 +0200

    Body formatted, JWT Token doesnt work and need to figure out how to run the code even if you are not on a page

--- src/assets/js/controllers/emailController.js
+++ src/assets/js/controllers/emailController.js
@@ -11,16 +11,43 @@ export class emailController extends Controller {
     constructor() {
         super();
         this.#emailRepository = new emailRepository();
-        // Sends an email every day at 7 am
-        cron.schedule('00 7 * * *', async () => {
-            const data = await this.#emailRepository.getEmail();
-            const subject = "Reminder" // Subject should be the same for every user
+        const minutes = "00" // Specified on which minute
+        const hours = "7" // Specified on which hour
+        // Sends an email every day at specific time
+        cron.schedule(`${minutes} ${hours} * * *`, async () => {
+            await this.#sendEmail()
+        })
 
-            for (let i = 0; i < data.data.length; i++) { // Sends email to every registered email
-                const userGoals = await this.#emailRepository.getUserGoals(data.data.id);
-
-                await this.#emailRepository.sendEmail(data.data[i].firstname, data.data[i].surname, data.data[i].emailAddress, subject, userGoals)
-            }
-        });
     }
+
+    async #sendEmail() {
+        const data = await this.#emailRepository.getEmail();
+        const subject = "Reminder"; // Subject should be the same for every user
+
+        for (let i = 0; i < data.data.length; i++) { // Sends email to every registered email
+            const userGoalsData = await this.#emailRepository.getUserGoals(data.data[i].id);
+
+            // Create an empty array to store the string parts
+            let stringBuilder = [];
+
+            for (let j = 0; j < userGoalsData.data.length; j++) {
+                // Append strings using push()
+                stringBuilder.push("\n"); // Empty line
+                stringBuilder.push(userGoalsData.data[j].name);
+                stringBuilder.push(": ");
+                stringBuilder.push(userGoalsData.data[j].valueChosenByUser);
+                stringBuilder.push(" ");
+                stringBuilder.push(userGoalsData.data[j].unit);
+                stringBuilder.push("\n"); // Empty line
+            }
+
+            // Join the array elements into a single string
+            let result = stringBuilder.join("");
+
+            console.log(result); // Example output: "peulvruchten eten: 50 gram"
+
+            await this.#emailRepository.sendEmail(data.data[i].firstname, data.data[i].surname, data.data[i].emailAddress, subject, result);
+        }
+    }
+
 }
