815de8a8ee4651065c662e1da63535d59e1c1475
Junior Brito <junior.br.pr@gmail.com>
2023-05-27 14:37:48 +0200

    - Nutrition recommendation improvement.

--- server/routes/recommendationsRoute.js
+++ server/routes/recommendationsRoute.js
@@ -13,37 +13,40 @@ class recommendationsRoute {
         this.#app.get("/recommendations/nutrition", this.#JWTHelper.verifyJWTToken, async (req, res) => {
 
             try {
-               const recommendedCategories = await this.#databaseHelper.handleQuery({
-                    query: `SELECT nc.name                      AS categoryName,
-                                     nc.recommended_weekly_intake AS recommendedWeeklyIntake
-                            FROM answer
-                                     INNER JOIN question ON question.id = answer.questionId
-                                     INNER JOIN response ON response.id = answer.responseId
-                                     INNER JOIN activity ON activity.id = question.activityId
-                                     INNER JOIN nutrition_activity_attribute naa ON activity.id = naa.activityId
-                                     INNER JOIN nutrition_category nc ON naa.category_id = nc.id
-                            WHERE question.activityId IS NOT NULL
-                              AND userId = ?
-                            GROUP BY categoryName
-                            HAVING SUM(answer.answer) < recommendedWeeklyIntake;`,
-                    values: [req.user.userId]
-                });
-
                 const recommendedActivities = await this.#databaseHelper.handleQuery({
                     query: `SELECT activity.id,
-                                        activity.name,
-                                        activity.description,
-                                        activity.unit,
-                                        nutrition_category.recommended_weekly_intake AS recommendedValue
-                                FROM activity
-                                            INNER JOIN nutrition_activity_attribute naa ON activity.id = naa.activityId
-                                            INNER JOIN nutrition_category ON naa.category_id = nutrition_category.id
-                                WHERE nutrition_category.name IN (?);`,
-                    values: [recommendedCategories.map(category => category.categoryName)]
+                                   activity.name,
+                                   activity.description,
+                                   activity.unit,
+                                   nutrition_category.recommended_weekly_intake AS recommendedValue
+                            FROM activity
+                                     INNER JOIN nutrition_activity_attribute naa
+                                                ON activity.id = naa.activityId
+                                     INNER JOIN nutrition_category
+                                                ON naa.category_id = nutrition_category.id
+                            WHERE nutrition_category.name
+                                      IN (SELECT recommended_categories.categoryName
+                                          FROM (SELECT nc.name                      AS categoryName,
+                                                       nc.recommended_weekly_intake AS recommendedWeeklyIntake,
+                                                       SUM(answer.answer)           AS weeklyUserIntake
+                                                FROM answer
+                                                         INNER JOIN question
+                                                                    ON question.id = answer.questionId
+                                                         INNER JOIN response
+                                                                    ON response.id = answer.responseId
+                                                         INNER JOIN activity
+                                                                    ON activity.id = question.activityId
+                                                         INNER JOIN nutrition_activity_attribute naa
+                                                                    ON activity.id = naa.activityId
+                                                         INNER JOIN nutrition_category nc
+                                                                    ON naa.category_id = nc.id
+                                                WHERE question.activityId IS NOT NULL
+                                                  AND userId = ?
+                                                GROUP BY categoryName
+                                                HAVING weeklyUserIntake < recommendedWeeklyIntake)
+                                                   AS recommended_categories);`,
+                    values: [req.user.userId]
                 });
-                console.log(recommendedActivities);
-                const r = {};
-
                 res.status(this.#errorCodes.HTTP_OK_CODE).json(recommendedActivities);
             } catch (e) {
                 res.status(this.#errorCodes.BAD_REQUEST_CODE).json({reason: e});
