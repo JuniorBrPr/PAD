b9caca29c9b616c4782a9f8e1504552126689dec
Junior Brito <junior.br.pr@gmail.com>
2023-05-28 23:28:41 +0200

    - Nutrition activities won't be recommended if the user already has aa user goal based on it

--- server/routes/recommendationsRoute.js
+++ server/routes/recommendationsRoute.js
@@ -26,27 +26,30 @@ class recommendationsRoute {
                                      INNER JOIN nutrition_category
                                                 ON naa.category_id = nutrition_category.id
                             WHERE nutrition_category.name
-                                      IN (SELECT recommended_categories.categoryName
-                                          FROM (SELECT nc.name                      AS categoryName,
-                                                       nc.recommended_weekly_intake AS recommendedWeeklyIntake,
-                                                       SUM(answer.answer)           AS weeklyUserIntake
-                                                FROM answer
-                                                         INNER JOIN question
-                                                                    ON question.id = answer.questionId
-                                                         INNER JOIN response
-                                                                    ON response.id = answer.responseId
-                                                         INNER JOIN activity
-                                                                    ON activity.id = question.activityId
-                                                         INNER JOIN nutrition_activity_attribute naa
-                                                                    ON activity.id = naa.activityId
-                                                         INNER JOIN nutrition_category nc
-                                                                    ON naa.category_id = nc.id
-                                                WHERE question.activityId IS NOT NULL
-                                                  AND userId = ?
-                                                GROUP BY categoryName
-                                                HAVING weeklyUserIntake < recommendedWeeklyIntake)
-                                                   AS recommended_categories);`,
-                    values: [req.user.userId]
+                                IN (SELECT recommended_categories.categoryName
+                                    FROM (SELECT nc.name                      AS categoryName,
+                                                 nc.recommended_weekly_intake AS recommendedWeeklyIntake,
+                                                 SUM(answer.answer)           AS weeklyUserIntake
+                                          FROM answer
+                                                   INNER JOIN question
+                                                              ON question.id = answer.questionId
+                                                   INNER JOIN response
+                                                              ON response.id = answer.responseId
+                                                   INNER JOIN activity
+                                                              ON activity.id = question.activityId
+                                                   INNER JOIN nutrition_activity_attribute naa
+                                                              ON activity.id = naa.activityId
+                                                   INNER JOIN nutrition_category nc
+                                                              ON naa.category_id = nc.id
+                                          WHERE question.activityId IS NOT NULL
+                                            AND userId = ?
+                                          GROUP BY categoryName
+                                          HAVING weeklyUserIntake < recommendedWeeklyIntake)
+                                             AS recommended_categories)
+                              AND activity.id NOT IN (SELECT usergoal.activityId
+                                                      FROM usergoal
+                                                      WHERE usergoal.userId = ?);`,
+                    values: [req.user.userId, req.user.userId]
                 });
                 res.status(this.#errorCodes.HTTP_OK_CODE).json(recommendedActivities);
             } catch (e) {
