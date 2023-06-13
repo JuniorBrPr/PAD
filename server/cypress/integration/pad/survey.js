028cddba25a0171cd236fbea78aa86dcccf765ef
Junior Brito <junior.br.pr@gmail.com>
2023-06-13 10:29:07 +0200

    - Tests and code improvements

--- server/cypress/integration/pad/survey.js
+++ server/cypress/integration/pad/survey.js
@@ -5,14 +5,26 @@
  */
 
 describe("Survey", () => {
+    Cypress.on('uncaught:exception', () => {
+        return false
+    });
+
     beforeEach(() => {
-        //Go to the specified URL
-        cy.visit("http://localhost:8080");
-        //Log in
-        cy.get("#logInNav").click();
-        cy.get("#exampleInputEmailAddress").type("test1");
-        cy.get("#exampleInputPassword").type("test1");
-        cy.get("#logInButton").click();
+        cy.intercept('POST', '/users/login', {
+            statusCode: 200,
+            body: {"accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIwMCwiZmlyc3RuYW1lIjoiSm9leVBlcm1LZXkiLCJyb2xlIjoxfQ.xXSJo2LZFyLbR_HbSg1Dwd83VuODwKyXKwu0uPrJ76Q"},
+        }).as('login');
+
+        cy.intercept('GET', '/users/isAdmin', {
+            statusCode: 200,
+            body: {"isAdmin": false},
+        }).as('isAdmin');
+
+        cy.visit("http://localhost:8080/#login");
+
+        cy.get("#InputEmailAddress").type("test");
+        cy.get("#InputPassword").type("test");
+        cy.get(".login-form button").click();
     });
 
     /**
@@ -29,7 +41,7 @@ describe("Survey", () => {
     it("Nutrition survey completed", () => {
         cy.server();
         const mockedResponse = [{"id": 2}];
-        cy.intercept('GET', '/survey/answered/1', {
+        cy.intercept('GET', '/survey/answered', {
             statusCode: 200,
             body: mockedResponse,
         }).as('unansweredSurvey');
@@ -52,7 +64,7 @@ describe("Survey", () => {
     it("Activity survey completed", () => {
         cy.server();
         const mockedResponse = [{"id": 1}];
-        cy.intercept('GET', '/survey/answered/1', {
+        cy.intercept('GET', '/survey/answered', {
             statusCode: 200,
             body: mockedResponse,
         }).as('unansweredSurvey');
@@ -114,6 +126,10 @@ describe("Survey", () => {
                 "open": 1
             }
         ];
+        cy.intercept('GET', '/survey/answered', {
+            statusCode: 200,
+            body: [{id: 1}, {id: 2}],
+        }).as('answeredSurvey');
 
         cy.intercept('GET', '/survey/nutrition/1', {
             statusCode: 200,
@@ -131,9 +147,63 @@ describe("Survey", () => {
 
         cy.get("#surveysNav").click();
         cy.get("#voeding-survey-btn").click();
-        cy.wait("@survey");
-        cy.wait("@options");
         cy.get("#formCheckBox").click();
         cy.get("#nextBtn").click();
+    });
+
+    /**
+     * Test if you can answer the activity survey.
+     */
+    it("Answer activity survey", () => {
+        cy.server();
+        const mockedResponseSurvey = [
+            {
+                id: 80,
+                surveyId: 2,
+                text: "Geef aan hoe vaak je per week naar een activiteit gaat en hoe lang je er over doet om er te " +
+                    "komen en weer terug te gaan.Bijvoorbeeld mantel-zorg, oppassen, vrijwilligerswerk, cursus volgen etc.",
+                type: "weeklyRecurringActivity"
+            },
+        ];
+        const mockedResponseOption = [
+            {
+                id: 192,
+                open: 0,
+                questionId: 80,
+                text: "Wandelen van/naar deze activiteit"
+            },
+            {
+                id: 193,
+                open: 0,
+                questionId: 80,
+                text: "Fietsen van/naar deze activiteit"
+            }
+        ];
+        cy.intercept('GET', '/survey/answered', {
+            statusCode: 200,
+            body: [{id: 1}, {id: 2}],
+        }).as('answeredSurvey');
+
+        cy.intercept('GET', '/survey/questions/2', {
+            statusCode: 200,
+            body: mockedResponseSurvey,
+        }).as('survey');
+
+        cy.intercept('GET', '/survey/options/80', {
+            statusCode: 200,
+            body: mockedResponseOption,
+        }).as('options');
+
+        cy.intercept('POST', '/survey/answer', {
+            statusCode: 200,
+        });
+
+        cy.get("#surveysNav").click();
+        cy.get("#beweging-survey-btn").click();
+        cy.get(".questionTab > :nth-child(1) > .text-center")
+            .should("be.visible")
+            .should("have.text", "Geef aan hoe vaak je per week naar een activiteit gaat en hoe " +
+                "lang je er over doet om er te komen en weer terug te gaan.Bijvoorbeeld mantel-zorg, oppassen, " +
+                "vrijwilligerswerk, cursus volgen etc.");
     });
 });
